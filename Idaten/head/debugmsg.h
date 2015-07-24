9eb74ad078740da792a2e067432957acbdf1cb9f
kaneko <iwgm14cyancle925@gmail.com>
2015-07-24 12:36:05 +0900

    Floatに対応できるようになりました

--- Idaten/head/debugmsg.h
+++ Idaten/head/debugmsg.h
@@ -1,6 +1,8 @@
 #pragma once
 #include<Windows.h>
+#include<iostream>
 
+#define DebugStringFloat(form,field,hdc,x,y,font_s) {TCHAR disp_msg[100];swprintf((LPWSTR)disp_msg, TEXT(form), field),Str_Put(hdc, x, y, font_s, RGB(255, 255, 255), disp_msg);}
 #define DebugStringVal(form,field,hdc,x,y,font_s) {TCHAR disp_msg[100];wsprintf((LPWSTR)disp_msg, TEXT(form), field),Str_Put(hdc, x, y, font_s, RGB(255, 255, 255), disp_msg);}
 #define DebugString(form,hdc,x,y,font_s) {TCHAR disp_msg[100];wsprintf((LPWSTR)disp_msg, TEXT(form)),Str_Put(hdc, x, y, font_s, RGB(255, 255, 255), disp_msg);}
 
