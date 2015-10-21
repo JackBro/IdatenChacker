0d49cf79123284ea8a211b99aa1434279aa566ad
kaneko <iwgm14cyancle@gmail.com>
2015-10-21 13:24:58 +0900

    パスを通しました以上

--- Idaten/src/Item/Itemupper.cpp
+++ Idaten/src/Item/Itemupper.cpp
@@ -5,7 +5,7 @@ Itemupper::Itemupper(int x,int y)
 {
 	item.x = x;
 	item.y = y;
-	 item_hb = (HBITMAP)LoadImage(NULL, TEXT("itemup.bmp"), IMAGE_BITMAP,
+	 item_hb = (HBITMAP)LoadImage(NULL, TEXT("res/Item/itemup.bmp"), IMAGE_BITMAP,
 		0, 0, LR_LOADFROMFILE | LR_CREATEDIBSECTION);
 	 itemtype = 1;
 	 item.onActive = 1;
