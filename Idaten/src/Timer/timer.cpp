7a952bdb6045167a77a4a976937e54531bd6fe8c
kaneko <iwgm14cyancle@gmail.com>
2016-01-22 10:27:04 +0900

    Timer

--- Idaten/src/Timer/timer.cpp
+++ Idaten/src/Timer/timer.cpp
@@ -14,7 +14,7 @@ void Timer::StartTimer(HDC hdc){
 void Timer::ShowTime(HDC hdc){
 	S_time = (timewindows / 100) / 10.0 - (timestart / 100) / 10.0;					//windows起動-プログラムが起動した時
 
-	DebugStringFloat("Time:%.1f秒", S_time, hdc, 950, 10, 20);
+	DebugStringFloatR("Time:%.1f秒", S_time, hdc, 850, 10, 40,-80);
 	return;
 }
 
